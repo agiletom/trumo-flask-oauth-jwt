ccd95cad78e82d9448d21b7032e4cea85fc423aa
yura <yurasenky.star@gmail.com>
2024-02-26 18:28:38 +0200

    add provder callback

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import secrets
+import requests
 from urllib.parse import urlencode
 
 from dotenv import load_dotenv
-from flask import Flask, redirect, url_for, render_template, session, current_app, abort
+from flask import Flask, redirect, url_for, render_template, flash, session, current_app, request, abort
 from pymongo import MongoClient
 from os import environ as env
+from flask_jwt_extended import create_access_token
 
 load_dotenv()
 
@@ -61,6 +63,53 @@ def oauth2_authorize(provider):
     })
 
     return redirect(provider_data['authorize_url'] + '?' + qs)
+
+@app.route('/callback/<provider>')
+def oauth2_callback(provider):
+    provider_data = current_app.config['OAUTH2_PROVIDERS'].get(provider)
+    if provider_data is None:
+        abort(404)
+
+    if 'error' in request.args:
+        for k, v in request.args.items():
+            if k.startswith('error'):
+                flash(f'{k}: {v}')
+        return redirect(url_for('index'))
+
+    if request.args['state'] != session.get('oauth2_state'):
+        abort(401)
+
+    if 'code' not in request.args:
+        abort(401)
+
+    response = requests.post(provider_data['token_url'], data={
+        'client_id': provider_data['client_id'],
+        'client_secret': provider_data['client_secret'],
+        'code': request.args['code'],
+        'grant_type': 'authorization_code',
+        'redirect_uri': url_for('oauth2_callback', provider=provider, _external=True),
+    }, headers={'Accept': 'application/json'})
+
+    if response.status_code != 200:
+        abort(401)
+    oauth2_token = response.json().get('access_token')
+    if not oauth2_token:
+        abort(401)
+
+    response = requests.get(provider_data['userinfo']['url'], headers={
+        'Authorization': 'Bearer ' + oauth2_token,
+        'Accept': 'application/json',
+    })
+    if response.status_code != 200:
+        abort(401)
+    email = provider_data['userinfo']['email'](response.json())
+
+    user = db.users.find_one()
+
+    if not user: 
+        user = db.users.insert_one({"email": email})
+
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=3000)
