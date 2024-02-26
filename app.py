6c40091a93de234484c3cfe9b9aaf639d50c7b83
yura <yurasenky.star@gmail.com>
2024-02-26 18:24:18 +0200

    add google, github provider

---
+++ app.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+from flask import Flask, render_template
+from os import environ as env
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top secret!'
+app.config['OAUTH2_PROVIDERS'] = {
+    'google': {
+        'client_id': env.get('GOOGLE_CLIENT_ID'),
+        'client_secret': env.get('GOOGLE_CLIENT_SECRET'),
+        'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
+        'token_url': 'https://accounts.google.com/o/oauth2/token',
+        'userinfo': {
+            'url': 'https://www.googleapis.com/oauth2/v3/userinfo',
+            'email': lambda json: json['email'],
+        },
+        'scopes': ['https://www.googleapis.com/auth/userinfo.email'],
+    },
+
+    'github': {
+        'client_id': env.get('GITHUB_CLIENT_ID'),
+        'client_secret': env.get('GITHUB_CLIENT_SECRET'),
+        'authorize_url': 'https://github.com/login/oauth/authorize',
+        'token_url': 'https://github.com/login/oauth/access_token',
+        'userinfo': {
+            'url': 'https://api.github.com/user/emails',
+            'email': lambda json: json[0]['email'],
+        },
+        'scopes': ['user:email'],
+    },
+}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=3000)
